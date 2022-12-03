a8d7d17f3c9dfb0b7bfa63c46ea6814bccc37308
Uchannel92 <uchenna_attah@hotmail.com>
2022-12-03 18:03:29 +0000

    removed commnets

--- exercise_1.py
+++ exercise_1.py
@@ -11,8 +11,3 @@ def when_will_you_turn_onehundred(name, age, hundrdyearsofage=100, year=2022):
 	return year_when_100
 
 my_answer = when_will_you_turn_onehundred('uchenna', 5)
-# print(my_answer)
-
-# Create a program that asks the user to enter their name and their age. 
-# Print out a message addressed to them that tells them the year that they will turn 100 years old. 
-# Note: for this exercise, the expectation is that you explicitly write out the year (and therefore be out of date the next year). If you want to do this in a generic way, see exercise 39.
